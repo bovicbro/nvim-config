75247661892772d9d1084e96a9c58badea9f8348
Victor Broman <victor.bo.broman@gmail.com>
2023-05-08 12:42:25 +0200

    adds rooter
    Rooter changes cwd to the first .git file it can find

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,10 @@
+return {
+  -- "nvim-neo-tree/neo-tree.nvim",
+  -- opts = {
+  --   filesystem = {
+  --     bind_to_cwd = false,
+  --     follow_current_file = false,
+  --     use_libuv_file_watcher = true,
+  --   },
+  -- },
+}
