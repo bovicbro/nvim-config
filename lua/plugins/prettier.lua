ff6665716345f09f968e35e7fc86c7a6779389f4
Victor Broman <victor.bo.broman@gmail.com>
2023-06-15 17:06:49 +0200

    adds new plugs

---
+++ lua/plugins/prettier.lua
@@ -0,0 +1,11 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function(_, opts)
+    local nls = require("null-ls")
+    table.insert(opts.sources, nls.builtins.formatting.prettierd)
+  end,
+  "williamboman/mason.nvim",
+  opts = function(_, opts)
+    table.insert(opts.ensure_installed, "prettierd")
+  end,
+}
