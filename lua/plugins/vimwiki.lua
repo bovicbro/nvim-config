4517edabb48f3e505a6058c5936a623d59b72fe4
Victor Broman <victor.bo.broman@gmail.com>
2023-09-11 10:09:35 +0200

    adds vimwiki

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -4,6 +4,7 @@ return {
     { "<leader>nn", "<cmd>VimwikiIndex<cr>", desc = "Vim wiki index" },
     { "<leader>nc", "<cmd>VimwikiColorizeNormal<cr>", desc = "color" },
     { "<leader>nh", "<cmd>VimwikiColorizeNormal<cr>", desc = "color" },
+    { "<leader>nd", "<cmd>VimwikiDeleteFile<cr>", desc = "Delete file" },
   },
   cmd = function()
     vim.g.vimwiki_list = {
