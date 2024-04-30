d3d133e4817e59a9f0b6dc8d9c00b3a36ebdea00
Victor Broman <victor.bo.broman@gmail.com>
2024-04-30 10:09:34 +0200

    adds vim fugitive and some keybinds

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,6 @@
 return {
   "tpope/vim-fugitive",
+  keys = {
+    { "<leader>go", "<cmd>0G<CR>", desc = "Vim Fugitive status" },
+  },
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,5 +5,8 @@ return {
       section_separators = { left = "", right = "" },
       component_separators = { left = "", right = "" },
     },
+    sections = {
+      lualine_z = {},
+    },
   },
 }
