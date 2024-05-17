9b99da1a3e257f445147282e245b9d0fb07f57ad
Victor Broman <victor.bo.broman@gmail.com>
2024-05-17 23:30:52 +0200

    Revert "adds"
    This reverts commit e856761bcd79a5e3b03faec5f34913f947d7d6ca.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -36,7 +36,6 @@ return {
     indent = { enable = true },
     -- context_commentstring = { enable = true, enable_autocmd = true },
     ensure_installed = {
-      "scheme",
       "templ",
       "bash",
       "c",
