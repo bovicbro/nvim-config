8267de52c51940e78b7ef6664151ec3a683dddd6
Victor Broman <victor.bo.broman@gmail.com>
2024-10-21 14:03:27 +0200

    adds keybindings for neogit

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,8 +1,8 @@
 return {
   "tpope/vim-fugitive",
   keys = {
-    { "<leader>go", "<cmd>0G<CR>", desc = "Vim Fugitive status" },
-    { "<leader>gl", "<cmd>G log<CR>", desc = "Vim Fugitive status" },
-    { "<leader>gd", "<cmd>G difftool<CR>", desc = "Vim Fugitive status" },
+    -- { "<leader>go", "<cmd>0G<CR>", desc = "Vim Fugitive status" },
+    -- { "<leader>gl", "<cmd>G log<CR>", desc = "Vim Fugitive status" },
+    -- { "<leader>gd", "<cmd>G difftool<CR>", desc = "Vim Fugitive status" },
   },
 }
--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -7,5 +7,11 @@ return {
     -- Only one of these is needed.
     "nvim-telescope/telescope.nvim", -- optional
   },
+  keys = {
+    { "<leader>gg", "<cmd>Neogit kind=replace<CR>", desc = "Neogit" },
+    { "<leader>gd", "<cmd>Neogit diff<CR>", desc = "Neogit diff" },
+    { "<leader>gl", "<cmd>NeogitLogCurrent<CR>", desc = "Neogit Log" },
+    { "<leader>gc", "<cmd>Neogit commit<CR>", desc = "Neogit commit" },
+  },
   config = true,
 }
