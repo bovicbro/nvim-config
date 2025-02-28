89b68f866935459aecd12030bd9ea708519f7737
Victor Broman <victor.bo.broman@gmail.com>
2025-02-28 18:48:13 +0100

    changes notes location

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,7 +6,7 @@ vim.opt.relativenumber = false
 vim.opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
 vim.filetype.add({ extension = { templ = "templ" } })
 vim.g.snacks_animate = false
-vim.g.wiki_root = "~/Documents/wiki"
+vim.g.wiki_root = "~/Documents/notes"
 vim.g.wiki_link_creation = {
   link_type = "wiki-link-wiki",
 }
